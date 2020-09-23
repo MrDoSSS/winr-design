223349a9b6ffecec70dc9b46be3bb1019cacca47
Коротенко Артур <akorotenko@rnds.pro>
2020-09-23 22:33:22 +0300

    Add wds

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,16 +1,22 @@
 import { Config } from '@stencil/core'
+import { sass } from '@stencil/sass'
 import alias from '@rollup/plugin-alias'
 import path from 'path'
 
 export const config: Config = {
   namespace: 'winr-components',
+  globalStyle: 'src/global/global.scss',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader',
+      copy: [
+        { src: 'global/fonts', warn: true, dest: 'fonts' }
+      ]
     },
     {
       type: 'dist-custom-elements-bundle',
+
     },
     {
       type: 'docs-readme',
@@ -18,6 +24,9 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
+      copy: [
+        { src: 'global/fonts', warn: true, dest: 'fonts' }
+      ]
     },
   ],
   plugins: [
@@ -25,6 +34,7 @@ export const config: Config = {
       entries: [
         { find: '@', replacement: path.resolve(path.resolve(__dirname), 'src') },
       ]
-    })
+    }),
+    sass()
   ]
 }
